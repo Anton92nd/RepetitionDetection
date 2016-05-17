dc5892aa0fc1533bf69544060848a944a00dee2a
Chaplygin Anton <anton92nd@skbkontur.ru>
2016-05-17 13:19:57 +0500

    critical factorization tests

---
+++ RepetitionDetection/Tests/CriticalFactorizationTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using RepetitionDetection.CriticalFactorization;
+
+namespace RepetitionDetection.Tests
+{
+    [TestFixture]
+    public class CriticalFactorizationTests
+    {
+        [TestCase("aba", 1)]
+        [TestCase("abaaaba", 2)]
+        [TestCase("aaaba", 3)]
+        [TestCase("abcdefgh", 7)]
+        [TestCase("abcabcdabc", 6)]
+        public void PrefixFactorizerTest(string template, int expectedPosition)
+        {
+            var factorizer = new PrefixFactorizer(template);
+            Assert.That(factorizer.GetCriticalFactorizationPosition(template.Length), Is.EqualTo(expectedPosition));
+        }
+
+        public void FactorizerTest(string pattern, int expectedPrefixLength, int expectedPrefixPosition, int expectedPatternPosition)
+        {
+            var factorization = Factorizer.GetFactorization(pattern);
+            Assert.That(factorization, Is.EqualTo(new Factorization(pattern, expectedPrefixLength, expectedPrefixPosition, expectedPatternPosition)));
+        }
+    }
+}
