b3cf439d5d845da2622d4625aa478cb49cbf1373
Chaplygin Anton <anton92nd@skbkontur.ru>
2016-05-20 18:24:44 +0500

    not constant space period calculator (using prefix function)

---
+++ RepetitionDetection/Periods/PeriodCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RepetitionDetection.Periods
+{
+    public static class PeriodCalculator
+    {
+        public static int GetPeriod(string pattern, int prefixLength)
+        {
+            var prefixFunction = new int[prefixLength];
+            prefixFunction[0] = 0;
+            var result = 0;
+            for (int i = 1; i < prefixLength; ++i)
+            {
+                var j = prefixFunction[i - 1];
+                while (j > 0  && pattern[i] != pattern[j])
+                {
+                    j = prefixFunction[j - 1];
+                }
+                if (pattern[i] == pattern[j])
+                    ++j;
+                prefixFunction[i] = j;
+                result = Math.Max(result, i - prefixFunction[i] + 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ RepetitionDetection/Tests/PeriodCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using RepetitionDetection.Periods;
+
+namespace RepetitionDetection.Tests
+{
+    [TestFixture]
+    public class PeriodCalculatorTests
+    {
+        [TestCase("abacaba", 4)]
+        [TestCase("abababab", 2)]
+        [TestCase("ddddd", 1)]
+        [TestCase("abcdef", 6)]
+        [TestCase("bacbac", 3)]
+        [TestCase("abaabc", 6)]
+        public void TestPatternPeriod(string template, int expectedPeriod)
+        {
+            Assert.That(PeriodCalculator.GetPeriod(template, template.Length), Is.EqualTo(expectedPeriod));
+        }
+    }
+}
