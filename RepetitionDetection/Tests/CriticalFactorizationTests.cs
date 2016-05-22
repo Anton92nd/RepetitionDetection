0ae5e64e0419f44a7ce3f72912156fa003be7c24
Anton92nd <anton92nd@gmail.com>
2016-05-22 18:05:03 +0600

    removed extra parameters in tests

--- RepetitionDetection/Tests/CriticalFactorizationTests.cs
+++ RepetitionDetection/Tests/CriticalFactorizationTests.cs
@@ -19,13 +19,14 @@ namespace RepetitionDetection.Tests
             Assert.That(factorizer.CriticalPosition, Is.EqualTo(expectedPosition));
         }
 
-        [TestCase("aaa", 0, 0, 3, 1)]       // (a, aa)
-        [TestCase("aba", 0, 0, 3, 1)]       // (a, ba)
-        [TestCase("abaaaba", 3, 1, 7, 2)]   // (a, ba) (ab, aaaba)
-        [TestCase("aaaba", 3, 1, 4, 3)]     // (a, aa) (aaa, ba)
-        [TestCase("abcdefgh", 4, 3, 5, 4)]  // (abc, d) (abcd, efgh)
-        [TestCase("abcabcdabc", 6, 2, 7, 6)]// (ab, cabc) (abcabc, dabc)
-        public void FactorizerTest(string pattern, int prefixLength, int prefixCriticalPosition, int patternPrefixLength, int patternCriticalPosition)
+        [TestCase("aaa", 0, 0, 1)]       // (a, aa)
+        [TestCase("aba", 0, 0, 1)]       // (a, ba)
+        [TestCase("abaaaba", 3, 1, 2)]   // (a, ba) (ab, aaaba)
+        [TestCase("aaaba", 3, 1, 3)]     // (a, aa) (aaa, ba)
+        [TestCase("abcdefgh", 4, 3, 4)]  // (abc, d) (abcd, efgh)
+        [TestCase("abcabcdabc", 6, 2, 6)]// (ab, cabc) (abcabc, dabc)
+        [TestCase("aaaaab", 5, 1, 5)]
+        public void FactorizerTest(string pattern, int prefixLength, int prefixCriticalPosition, int patternCriticalPosition)
         {
             var factorization = Factorizer.GetFactorization(pattern);
             Assert.That(factorization, Is.EqualTo(new Factorization(pattern, prefixLength, prefixCriticalPosition, patternCriticalPosition)));
