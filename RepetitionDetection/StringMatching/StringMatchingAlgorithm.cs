369c0ac72641af2981de8c37ba1235ff803a8d41
Anton92nd <anton92nd@gmail.com>
2016-05-22 19:36:27 +0600

    string matchign algo implemented

--- RepetitionDetection/StringMatching/StringMatchingAlgorithm.cs
+++ RepetitionDetection/StringMatching/StringMatchingAlgorithm.cs
@@ -1,15 +1,62 @@
 ﻿using System.Text;
 using RepetitionDetection.CriticalFactorization;
+using RepetitionDetection.Periods;
 
 namespace RepetitionDetection.StringMatching
 {
     public class StringMatchingAlgorithm
     {
-        public StringMatchingAlgorithm(StringBuilder text, string pattern)
+        public StringMatchingAlgorithm(StringBuilder text, string pattern, int startPosition)
         {
+            this.text = text;
             var factorizations = Factorizer.GetFactorization(pattern);
+            if (factorizations.PatternCriticalPosition > pattern.Length/2)
+            {
+                shift = pattern.Length - factorizations.PrefixLength;
+                incompleteAlgorithm = new IncompleteStringMatchingAlgorithm(text, startPosition, pattern, pattern.Length,
+                    factorizations.PatternCriticalPosition, PeriodCalculator.GetPeriod(pattern, pattern.Length));
+                completeAlgorithm = new CompleteStringMatchingAlgorithm(text, startPosition, pattern, factorizations.PrefixLength,
+                    factorizations.PrefixCriticalPosition, PeriodCalculator.GetPeriod(pattern, factorizations.PrefixLength));
+            }
+            else
+            {
+                shift = 0;
+                incompleteAlgorithm = null;
+                completeAlgorithm = new CompleteStringMatchingAlgorithm(text, startPosition, pattern, pattern.Length,
+                    factorizations.PatternCriticalPosition, PeriodCalculator.GetPeriod(pattern, pattern.Length));
+            }
         }
 
-        public AlgorithmState State { get; private set; }
+        public bool CheckForMatch()
+        {
+            if (incompleteAlgorithm == null)
+            {
+                return completeAlgorithm.CheckMatch(text.Length);
+            }
+            var incompleteAlgoResult = incompleteAlgorithm.CheckMatch(text.Length);
+            var completeAlgoResult = completeAlgorithm.CheckMatch(text.Length - shift);
+            return incompleteAlgoResult && completeAlgoResult;
+        }
+
+        public StringMatchingState State
+        {
+            get
+            {
+                return new StringMatchingState(incompleteAlgorithm == null ? new AlgorithmState(-1, -1) : incompleteAlgorithm.State, completeAlgorithm.State);
+            }
+        }
+
+        public void SetState(StringMatchingState state)
+        {
+            if (incompleteAlgorithm != null)
+                incompleteAlgorithm.SetState(state.IncompleteAlgorithmState);
+            completeAlgorithm.SetState(state.CompleteAlgorithmState);
+        }
+
+        private readonly IStringMatchingAlgorithm completeAlgorithm;
+        private readonly IStringMatchingAlgorithm incompleteAlgorithm;
+        private readonly int shift;
+
+        private readonly StringBuilder text;
     }
 }
---
+++ RepetitionDetection/StringMatching/StringMatchingState.cs
@@ -0,0 +1,14 @@
+﻿namespace RepetitionDetection.StringMatching
+{
+    public class StringMatchingState
+    {
+        public StringMatchingState(AlgorithmState incompleteAlgorithmState, AlgorithmState completeAlgorithmState)
+        {
+            IncompleteAlgorithmState = incompleteAlgorithmState;
+            CompleteAlgorithmState = completeAlgorithmState;
+        }
+
+        public AlgorithmState CompleteAlgorithmState { get; private set; }
+        public AlgorithmState IncompleteAlgorithmState { get; private set; }
+    }
+}
