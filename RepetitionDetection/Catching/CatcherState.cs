270058beff7fa7a4d75163cadf86af253cbc2f9d
Anton92nd <anton92nd@gmail.com>
2016-05-28 14:10:35 +0500

    created immutable catcher state

--- RepetitionDetection/Catching/CatcherState.cs
+++ RepetitionDetection/Catching/CatcherState.cs
@@ -1,6 +1,18 @@
-﻿namespace RepetitionDetection.Catching
+﻿using System.Collections.Immutable;
+using RepetitionDetection.Commons;
+using RepetitionDetection.StringMatching;
+
+namespace RepetitionDetection.Catching
 {
     public class CatcherState
     {
+        public CatcherState(ImmutableArray<Repetition> repetitions, StringMatchingState stringMatchingState)
+        {
+            Repetitions = repetitions;
+            StringMatchingState = stringMatchingState;
+        }
+
+        public StringMatchingState StringMatchingState { get; private set; }
+        public ImmutableArray<Repetition> Repetitions { get; private set; }
     }
 }
