07cd16c8c113c6fe8bba99f3c4b9bcd48826f6ff
Anton92nd <anton92nd@gmail.com>
2016-05-30 17:09:04 +0500

    test detector with backtrack

--- RepetitionDetection/Tests/LargeRepetitionDetectorTests.cs
+++ RepetitionDetection/Tests/LargeRepetitionDetectorTests.cs
@@ -34,5 +34,43 @@ namespace RepetitionDetection.Tests
                 new Repetition(3, 8), 
             }));
         }
+
+        [Test]
+        public void TestWithBacktrack()
+        {
+            var e = new RationalNumber(3, 2);
+            var text = "wxyzaceorsuvaceo";
+            var sb = new StringBuilder();
+
+            var detector = new LargeRepetitionsDetector(sb, e, true);
+            var repetitions = new List<Repetition>();
+
+            Repetition repetition;
+            for (var i = 0; i < text.Length; ++i)
+            {
+                
+                sb.Append(text[i]);
+                if (detector.TryDetect(out repetition))
+                {
+                    repetitions.Add(repetition);
+                }
+            }
+
+            Assert.That(repetitions, Is.EquivalentTo(new[]
+            {
+                new Repetition(3, 8), 
+            }));
+
+            detector.BackTrack();
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append('p');
+            Assert.That(detector.TryDetect(out repetition), Is.False);
+
+            detector.BackTrack();
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append('o');
+            Assert.That(detector.TryDetect(out repetition), Is.True);
+            Assert.That(repetition, Is.EqualTo(new Repetition(3, 8)));
+        }
     }
 }
