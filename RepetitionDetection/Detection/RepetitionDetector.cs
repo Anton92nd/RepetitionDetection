00fbc0e0b90471a7cb5980b782e615a18819eb02
Anton92nd <anton92nd@gmail.com>
2016-05-30 17:53:37 +0500

    repetition detector

---
+++ RepetitionDetection/Detection/RepetitionDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RepetitionDetection.Commons;
+
+namespace RepetitionDetection.Detection
+{
+    public class RepetitionDetector : IDetector
+    {
+        private readonly IDetector smallRepetitionsDetector;
+        private readonly IDetector largeRepetitionsDetector;
+
+        public RepetitionDetector(StringBuilder text, RationalNumber e, bool detectEqual)
+        {
+            if (text.Length > 0)
+            {
+                throw new InvalidUsageException("Text must be empty when creating LargeRepetitionDetector");
+            }
+            smallRepetitionsDetector = new SmallRepetitionDetector(text, e, detectEqual);
+            largeRepetitionsDetector = new LargeRepetitionDetector(text, e, detectEqual);
+        }
+
+        public bool TryDetect(out Repetition repetition)
+        {
+            var result = largeRepetitionsDetector.TryDetect(out repetition) || smallRepetitionsDetector.TryDetect(out repetition);
+            return result;
+        }
+
+        public void BackTrack()
+        {
+            largeRepetitionsDetector.BackTrack();
+            smallRepetitionsDetector.BackTrack();
+        }
+    }
+}
---
+++ RepetitionDetection/Tests/RepetitionDetectorTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using NUnit.Framework;
+using RepetitionDetection.Commons;
+using RepetitionDetection.Detection;
+
+namespace RepetitionDetection.Tests
+{
+    [TestFixture]
+    public class RepetitionDetectorTests
+    {
+        [TestCase("wxyzaceorsuvaceo", 3, 2, 3, 8, true)]
+        [TestCase("xxxxaceorsuvaceo", 3, 2, -1, 1, true)]
+        public void Test(string text, int num, int denom, int lp, int p, bool detectEqual)
+        {
+            var e = new RationalNumber(num, denom);
+            var sb = new StringBuilder();
+
+            var detector = new RepetitionDetector(sb, e, detectEqual);
+            var repetition = new Repetition(0, 0);
+
+            foreach (var c in text)
+            {
+                sb.Append(c);
+                if (detector.TryDetect(out repetition))
+                {
+                    break;
+                }
+            }
+
+            Assert.That(repetition, Is.EqualTo(new Repetition(lp, p)));
+        }
+
+        [Test]
+        public void TestWithBacktrack()
+        {
+            var e = new RationalNumber(3, 2);
+            var text = "wxyzaceorsuvaceo";
+            var sb = new StringBuilder();
+
+            var detector = new LargeRepetitionDetector(sb, e, true);
+            var repetition = new Repetition(0, 0);
+
+            for (var i = 0; i < text.Length; ++i)
+            {
+                sb.Append(text[i]);
+                if (detector.TryDetect(out repetition))
+                {
+                    break;
+                }
+            }
+
+            Assert.That(repetition, Is.EqualTo(new Repetition(3, 8)));
+
+            detector.BackTrack();
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append('p');
+            Assert.That(detector.TryDetect(out repetition), Is.False);
+
+            detector.BackTrack();
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append('o');
+            Assert.That(detector.TryDetect(out repetition), Is.True);
+            Assert.That(repetition, Is.EqualTo(new Repetition(3, 8)));
+        }
+    }
+}
