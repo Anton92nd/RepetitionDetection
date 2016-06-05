88209bd2cac331061ec4fc925d076b95de9a7dc9
Anton92nd <anton92nd@gmail.com>
2016-06-05 14:50:28 +0500

    hack with small repetitions

--- RepetitionDetection/Detection/RepetitionDetector.cs
+++ RepetitionDetection/Detection/RepetitionDetector.cs
@@ -7,6 +7,7 @@ namespace RepetitionDetection.Detection
     {
         private readonly IDetector smallRepetitionsDetector;
         private readonly IDetector largeRepetitionsDetector;
+        private bool SkipBacktrack;
 
         public RepetitionDetector(StringBuilder text, RationalNumber e, bool detectEqual)
         {
@@ -16,18 +17,27 @@ namespace RepetitionDetection.Detection
             }
             smallRepetitionsDetector = new SmallRepetitionDetector(text, e, detectEqual);
             largeRepetitionsDetector = new LargeRepetitionDetector(text, e, detectEqual);
+            SkipBacktrack = false;
         }
 
         public bool TryDetect(out Repetition repetition)
         {
-            var result = largeRepetitionsDetector.TryDetect(out repetition) || smallRepetitionsDetector.TryDetect(out repetition);
-            return result;
+            if (smallRepetitionsDetector.TryDetect(out repetition))
+            {
+                SkipBacktrack = true;
+                return true;
+            }
+            return largeRepetitionsDetector.TryDetect(out repetition);
         }
 
         public void BackTrack()
         {
-            largeRepetitionsDetector.BackTrack();
-            smallRepetitionsDetector.BackTrack();
+            if (!SkipBacktrack)
+            {
+                largeRepetitionsDetector.BackTrack();
+                smallRepetitionsDetector.BackTrack();
+            }
+            SkipBacktrack = false;
         }
     }
 }
