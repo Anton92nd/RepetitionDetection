c40841c8030d024b1baa9d1b74e852f8167d05ae
Anton92nd <anton92nd@gmail.com>
2016-06-10 12:50:55 +0500

    Generator loggers

---
+++ RepetitionDetection/Logging/ConsoleTextLengthLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RepetitionDetection.Commons;
+
+namespace RepetitionDetection.Logging
+{
+    public class ConsoleTextLengthLogger : IGeneratorLogger
+    {
+        private readonly int length;
+
+        public ConsoleTextLengthLogger(int lengthStep)
+        {
+            this.length = lengthStep;
+        }
+
+        public void LogBeforeGenerate(StringBuilder text)
+        {
+            if (text.Length % length == 0)
+                Console.Write("\rText length: {0}    ", text.Length);
+        }
+
+        public void LogAfterGenerate(StringBuilder text)
+        {
+        }
+
+        public void LogRepetition(StringBuilder text, Repetition repetition)
+        {
+        }
+    }
+}
---
+++ RepetitionDetection/Logging/IGeneratorLogger.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using RepetitionDetection.Commons;
+
+namespace RepetitionDetection.Logging
+{
+    public interface IGeneratorLogger
+    {
+        void LogBeforeGenerate(StringBuilder text);
+        void LogAfterGenerate(StringBuilder text);
+        void LogRepetition(StringBuilder text, Repetition repetition);
+    }
+}
