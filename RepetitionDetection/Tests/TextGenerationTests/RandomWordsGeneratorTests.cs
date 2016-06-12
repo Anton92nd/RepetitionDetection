416296731fe16bd4c17632de1a4841c35b8ddf99
Anton92nd <anton92nd@gmail.com>
2016-06-12 16:08:49 +0500

    tests clean up

--- RepetitionDetection/Tests/TextGenerationTests/RandomWordsGeneratorTests.cs
+++ RepetitionDetection/Tests/TextGenerationTests/RandomWordsGeneratorTests.cs
@@ -15,17 +15,16 @@ namespace RepetitionDetection.Tests.TextGenerationTests
     [TestFixture]
     public class RandomWordsGeneratorTests
     {
-        private static readonly int[] Lengths = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000 };
-
         [TestCase(4, 7, 4, true, typeof(RepetitionDetector))]
         [TestCase(4, 7, 4, false, typeof(RepetitionDetector))]
         [TestCase(4, 7, 4, true, typeof(SillyDetector))]
         [TestCase(4, 7, 4, false, typeof(SillyDetector))]
         public void TestDetector(int alphabet, int numerator, int denominator, bool detectEqual, Type detectorType)
         {
+            var lengths = new[] { 1000, 2000 };
             var generator = new RandomCharGenerator(alphabet);
             var detector = GetDetector(detectorType, detectEqual, new RationalNumber(numerator, denominator));
-            foreach (var length in Lengths)
+            foreach (var length in lengths)
             {
                 detector.Reset();
                 var sw = Stopwatch.StartNew();
@@ -47,8 +46,6 @@ namespace RepetitionDetection.Tests.TextGenerationTests
             Console.WriteLine("Length: {0}\n\tTime: {1} ms\n\tConversion coeff: {2:0.000000}", length, sw.ElapsedMilliseconds, RandomWordGenerator.CharsGenerated * 1.0 / length);
         }
 
-        private static readonly int[] LengthsBoundary = Enumerable.Range(1, 10).Select(n => n * 1000).ToArray();
-
         [TestCase(5)]
         [TestCase(6)]
         [TestCase(7)]
@@ -57,6 +54,7 @@ namespace RepetitionDetection.Tests.TextGenerationTests
         [TestCase(10)]
         public void TestBoundaryLanguages(int k)
         {
+            var lengthsBoundary = Enumerable.Range(1, 10).Select(n => n * 100).ToArray();
             var strategy = new RemoveBorderStrategy();
             var e = new RationalNumber(k, k - 1);
             var detector = new RepetitionDetector(new StringBuilder(), e, false);
@@ -65,7 +63,7 @@ namespace RepetitionDetection.Tests.TextGenerationTests
             var times = new List<long>();
             var coefs = new List<double>();
             var sw = Stopwatch.StartNew();
-            foreach (var length in LengthsBoundary)
+            foreach (var length in lengthsBoundary)
             {
                 RandomWordGenerator.Generate(detector, length, strategy, generator);
                 charsGenerated += RandomWordGenerator.CharsGenerated;
@@ -74,9 +72,9 @@ namespace RepetitionDetection.Tests.TextGenerationTests
             }
             sw.Stop();
             Console.WriteLine("Alphabet size = {0}, e = {1}+", k, e);
-            for (var i = 0; i < LengthsBoundary.Length; ++i)
+            for (var i = 0; i < lengthsBoundary.Length; ++i)
             {
-                Console.WriteLine("\tLength: {0}, Time: {1} ms, Conversion coeff: {2}", LengthsBoundary[i], times[i], coefs[i]);
+                Console.WriteLine("\tLength: {0}, Time: {1} ms, Conversion coeff: {2}", lengthsBoundary[i], times[i], coefs[i]);
             }
         }
 
