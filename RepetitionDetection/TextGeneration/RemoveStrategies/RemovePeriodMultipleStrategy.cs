1843f3e66f8fdbe46a81b42c65aba80cfa777ee8
Anton92nd <anton92nd@gmail.com>
2016-06-17 13:25:52 +0500

    RemovePeriodStrategy

---
+++ RepetitionDetection/TextGeneration/RemoveStrategies/RemovePeriodMultipleStrategy.cs
@@ -0,0 +1,19 @@
+﻿using RepetitionDetection.Commons;
+
+namespace RepetitionDetection.TextGeneration.RemoveStrategies
+{
+    public class RemovePeriodMultipleStrategy : IRemoveStrategy
+    {
+        private readonly int periodsCount;
+
+        public RemovePeriodMultipleStrategy(int periodsCount = 1)
+        {
+            this.periodsCount = periodsCount;
+        }
+
+        public int GetCharsToDelete(int textLength, Repetition repetition)
+        {
+            return repetition.Period*periodsCount;
+        }
+    }
+}
