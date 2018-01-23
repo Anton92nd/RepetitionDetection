d8f1eecbcf90bd90e5d05deaaf3b80c8cc3b09df
Anton Chaplygin <anton92nd@gmail.com>
2018-01-24 00:36:26 +0500

    Added custom remove strategy for experiments

---
+++ RepetitionDetection/TextGeneration/RemoveStrategies/CustomRemoveStrategy.cs
@@ -0,0 +1,23 @@
+﻿using RepetitionDetection.Commons;
+
+namespace RepetitionDetection.TextGeneration.RemoveStrategies
+{
+    public class CustomRemoveStrategy : IRemoveStrategy
+    {
+        public int GetCharsToDelete(int textLength, Repetition repetition)
+        {
+            if (repetition.Period >= 12 && repetition.Period <= 30)
+            {
+                return 9;
+            }
+            return baseStrategy.GetCharsToDelete(textLength, repetition);
+        }
+
+        public override string ToString()
+        {
+            return $"NoMoreThanStrategy([12, 30], 9, {baseStrategy})";
+        }
+
+        private readonly IRemoveStrategy baseStrategy = new RemoveBorderStrategy();
+    }
+}
