d5b4063074d0aaf36cabaa1b23bc72efe3585bb3
Anton Chaplygin <anton92nd@gmail.com>
2018-03-25 15:51:52 +0500

    Repetition equality members

--- RepetitionDetection/Commons/Repetition.cs
+++ RepetitionDetection/Commons/Repetition.cs
@@ -13,14 +13,34 @@ namespace RepetitionDetection.Commons
             Period = period;
         }
 
+        public override bool Equals(object obj)
+        {
+            return obj is Repetition r && Equals(r);
+        }
+
         public bool Equals(Repetition other)
         {
             return LeftPosition == other.LeftPosition && Period == other.Period;
         }
 
+        public override int GetHashCode()
+        {
+            return LeftPosition * 997 + Period.GetHashCode();
+        }
+
         public override string ToString()
         {
             return $"Left position: {LeftPosition}, Period: {Period}";
+        }
+
+        public static bool operator ==(Repetition first, Repetition second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Repetition first, Repetition second)
+        {
+            return !(first == second);
         }
     }
 }
